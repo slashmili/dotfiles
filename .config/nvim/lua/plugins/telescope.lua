b5fcd322086a7ac8ca443aedd0750248adbd41c3
Milad <milad@high-mobility.com>
2023-12-04 02:01:10 +0100

    resume telescope search with option+p

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
 	{"<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "live grep"},
 	{"<leader>b", "<cmd>Telescope buffers theme=dropdown previewer=false<cr>", desc = "find from buffers"},
 	{"<C-p>", "<cmd>Telescope git_files theme=dropdown previewer=false<cr>", desc = "find git files"},
+	{"π", "<cmd>Telescope resume<cr>", desc = "resume search by option+p"},
       },
       config = function()
         require "telescope".setup {
