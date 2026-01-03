733fffc6a0863a9fc113737fb56e137415c8dbb8
Milad Rastian <milad@high-mobility.com>
2026-01-03 21:00:11 +0100

    new k8s alias to execute under a pod

--- .config/fish/functions/kbash.fish
+++ .config/fish/functions/kbash.fish
@@ -2,3 +2,15 @@ function kbash
 kubectl wait --for=condition=Ready pod/$argv
 kubectl exec -it $argv -- bash
 end
+
+function __fish_kbash_targets
+    kubectl get pods,deploy,sts,rs,job \
+        --all-namespaces \
+        --no-headers \
+        -o custom-columns='KIND:.kind,NAME:.metadata.name,NAMESPACE:.metadata.namespace' 2>/dev/null \
+    | awk '{ print tolower($1) "/" $2 "\t" $3 }'
+end
+
+complete -c kbash -s n -l namespace -r -a "(__fish_kubectl_get_namespaces)" -d "Namespace"
+complete -c kbash -f -a "(__fish_kbash_targets)" -d "Target resource"
+
